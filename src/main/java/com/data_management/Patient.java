6bd2c228552738b867a3db03da5f587b5d5ab54a
Alex100424 <alexandru.creciun@student.maastrichtuniversity.nl>
2026-04-09 22:28:58 +0200

    Implemented the getRecords method

--- src/main/java/com/data_management/Patient.java
+++ src/main/java/com/data_management/Patient.java
@@ -53,5 +53,12 @@ public class Patient {
      */
     public List<PatientRecord> getRecords(long startTime, long endTime) {
         // TODO Implement and test this method
+        List<PatientRecord> filteRecords = new ArrayList<>();
+        for(PatientRecord record : patientRecords)
+        {
+            if(record.getTimestamp() >= startTime && record.getTimestamp() <= endTime)
+                filteRecords.add(record);
+        }
+        return filteRecords;
     }
 }
